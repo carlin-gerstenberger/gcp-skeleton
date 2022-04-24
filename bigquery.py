ca5a7e51e754f74907fa6624309864f828297682
Carlin Gerstenberger <66455820+carlin-gerstenberger@users.noreply.github.com>
2022-04-24 14:55:15 -0700

    Create bigquery.py

---
+++ bigquery.py
@@ -0,0 +1,14 @@
+import google
+from google.cloud import bigquery
+
+
+
+def bq(query):
+
+    client = bigquery.Client()
+
+    query_job = client.query(query)
+    df = query_job.to_dataframe()
+    json = df.to_json(orient='records')
+
+    return json
